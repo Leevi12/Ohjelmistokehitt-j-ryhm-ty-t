4aab3ccd0b90ef38161d900ed0114c7bfd8fd221
Leevi12 <leevi.sallantaus@gmail.com>
2021-11-23 11:36:40 +0200

    Tehtavan aloitus

---
+++ Hotelli/Hotelli/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace Hotelli
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SignInBT = new System.Windows.Forms.Button();
+            this.UsernameTB = new System.Windows.Forms.TextBox();
+            this.PasswordTB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SignInBT
+            // 
+            this.SignInBT.Location = new System.Drawing.Point(309, 220);
+            this.SignInBT.Name = "SignInBT";
+            this.SignInBT.Size = new System.Drawing.Size(140, 51);
+            this.SignInBT.TabIndex = 0;
+            this.SignInBT.Text = "Sign In";
+            this.SignInBT.UseVisualStyleBackColor = true;
+            // 
+            // UsernameTB
+            // 
+            this.UsernameTB.Location = new System.Drawing.Point(277, 69);
+            this.UsernameTB.Name = "UsernameTB";
+            this.UsernameTB.Size = new System.Drawing.Size(228, 26);
+            this.UsernameTB.TabIndex = 1;
+            // 
+            // PasswordTB
+            // 
+            this.PasswordTB.Location = new System.Drawing.Point(277, 138);
+            this.PasswordTB.Name = "PasswordTB";
+            this.PasswordTB.Size = new System.Drawing.Size(228, 26);
+            this.PasswordTB.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(166, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(171, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PasswordTB);
+            this.Controls.Add(this.UsernameTB);
+            this.Controls.Add(this.SignInBT);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.Name = "Form1";
+            this.Text = "Sign in";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SignInBT;
+        private System.Windows.Forms.TextBox UsernameTB;
+        private System.Windows.Forms.TextBox PasswordTB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
