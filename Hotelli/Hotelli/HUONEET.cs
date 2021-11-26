2f3d37d465353384d2c310da9d2c41fcab4db3e8
Qlueys <46287401+Qlueys@users.noreply.github.com>
2021-11-26 17:39:50 +0200

    blah blah blah

---
+++ Hotelli/Hotelli/HUONEET.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotelli
+{
+    class HUONEET
+    {
+        public bool lisaaHuone(int hnro, int hntyyppi, string puh, string vapaa)
+        {
+
+        }
+    }
+}
