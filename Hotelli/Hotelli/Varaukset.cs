7fd3b4dd8f6f0add02119df3283ad26d3e385cb4
Leevi12 <leevi.sallantaus@gmail.com>
2021-12-01 10:50:32 +0200

    vielä kesken

--- Hotelli/Hotelli/Varaukset.cs
+++ Hotelli/Hotelli/Varaukset.cs
@@ -25,26 +25,58 @@ namespace Hotelli
         private void TallVrBT_Click(object sender, EventArgs e)
         {
             int asiakas = Convert.ToInt32(AsiakasCB.SelectedValue.ToString());
-            int huone = Convert.ToInt32(HuoTyypCB.SelectedValue.ToString());
+            int huone = Convert.ToInt32(HuoneTyyppiCB.SelectedValue.ToString());
             DateTime sisaankirjautuminen = Convert.ToDateTime(SisaanDTP.Value);
             DateTime uloskirjautuminen = Convert.ToDateTime(UlosDTP.Value);
-            
+
             if (varaus.addVaraus(huone, asiakas, sisaankirjautuminen, uloskirjautuminen))
             {
                 MessageBox.Show("Varauksen lisäys onnistui", "Varaus", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
-                else
+            else
             {
-                    MessageBox.Show("Varauksen lisäys epäonnistui", "Varaus epäonnistui", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Varauksen lisäys epäonnistui", "Varaus epäonnistui", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             VarauksetDG.DataSource = varaus.haeVaraukset();
         }
+
+        private void Varaukset_Load(object sender, EventArgs e)
+        {
+            HuoneTyyppiCB.DataSource = huone.haeHuoneet();
+            HuoneTyyppiCB.DisplayMember = "Huonetyyppi";
+            HuoneTyyppiCB.ValueMember = "KategoriaId";
+
+            AsiakasCB.DataSource = asiakas.getAsiakkaat();
+            AsiakasCB.DisplayMember = "Kokonimi";
+            AsiakasCB.ValueMember = "AsiakasId";
+            VarauksetDG.DataSource = varaus.haeVaraukset();
+
+        }
+        
+        private void HuoneTyyppiCB_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int huonetype = HuoneTyyppiCB.SelectedIndex + 1;
+            
+            HuoneNroCB.DataSource = huone.tyypillisethuoneet(huonetype);
+            HuoneNroCB.DisplayMember = "Huoneen numero";
+            HuoneNroCB.ValueMember = "Huoneen numero";
+        }
+
+        private void MuokVrBT_Click(object sender, EventArgs e)
+        {
+            int huone = Convert.ToInt32(HuoneNroCB.SelectedValue.ToString());
+            int asiakas = Convert.ToInt32(AsiakasCB.SelectedValue.ToString());
+            DateTime sisaan = Convert.ToDateTime(SisaanDTP.Value);
+            DateTime ulos = Convert.ToDateTime(UlosDTP.Value);
+            try
+            {
+                int vara = Convert.ToInt32(VrNumeroTB.Text);
+
+                if (varaus.editVaraus(huone, asiakas, sisaan, ulos, vara))
+                {
+                    MessageBox.Show("Huone muokattu onnistuneesti", "Huoneen muokkaus", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
     }
-        //}
-
-        //private void Varaukset_Load(object sender, EventArgs e)
-        //{
-
-        //}
-    //}
 }
